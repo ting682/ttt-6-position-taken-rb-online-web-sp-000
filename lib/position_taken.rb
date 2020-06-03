42d2e8ecb595061aa0d3c4885fe4f8d8f6d87f6b
Ting Chung <tchung682@gmail.com>
2020-06-03 17:55:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
 
-  if board[index] == " "
+  if board[index] == " " || board[index] == ""
     false
-  elsif board[index] == ""
-    false
+  elsif 
+    
 
   end
 
